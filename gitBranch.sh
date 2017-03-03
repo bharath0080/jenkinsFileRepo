97bfce8fd068a4224467b55f60e784281e008199
root <root@10.242.138.107>
2017-03-03 12:16:28 +0530

    Git tocken change

--- gitBranch.sh
+++ gitBranch.sh
@@ -7,12 +7,12 @@ if [ $1 == "branchCreation" ]
 then
 
         SHA_ID=`curl -s  https://api.github.com/repos/Rajeshkumar90/jpetStore/commits/master | grep sha | head -1 | awk -F":" '{print $2}'| sed -e 's/"//g' -e 's/,//g' -e 's/,//g' -e 's/ //g'`;
-        curl -X POST -u "Rajeshkumar90:81b159d2d193e425dd689511c41786695201c080" -d "{\"ref\": \"refs/heads/${STORY_ID}_branch\",\"sha\":\"$SHA_ID\"}" "https://api.github.com/repos/Rajeshkumar90/jpetStore/git/refs"
+        curl -X POST -u "Rajeshkumar90:27fdec6505768fdce690c14655c86f53573c6fbe" -d "{\"ref\": \"refs/heads/${STORY_ID}_branch\",\"sha\":\"$SHA_ID\"}" "https://api.github.com/repos/Rajeshkumar90/jpetStore/git/refs"
 
 elif [ $1 == "mergeApproval" ]
 then
         echo "Creating the pull request for merging the code from ${STORY_ID}_branch to master";
-        curl -X POST -u Rajeshkumar90:81b159d2d193e425dd689511c41786695201c080 -k -d '{"title": "Changes for '"${STORY_ID}"' project feature","head": "'"${STORY_ID}"'_branch","base": "master"}' "https://api.github.com/repos/Rajeshkumar90/jpetStore/pulls"
+        curl -X POST -u Rajeshkumar90:27fdec6505768fdce690c14655c86f53573c6fbe -k -d '{"title": "Changes for '"${STORY_ID}"' project feature","head": "'"${STORY_ID}"'_branch","base": "master"}' "https://api.github.com/repos/Rajeshkumar90/jpetStore/pulls"
 
 else
         echo "No valid argument is provided";
